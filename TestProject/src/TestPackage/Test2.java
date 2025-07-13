3885eb988efe05018023300f41c6b624bd02ebb4
Admin <Admin@Desktop>
2025-07-13 14:33:48 +0530

    Adding new class

---
+++ TestProject/src/TestPackage/Test2.java
@@ -0,0 +1,16 @@
+package TestPackage;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+
+		String a = "Gaon";
+		int b = 10;
+		
+		String gaon = "Daregaon";
+		
+		System.out.println(b + "Km"+" " + gaon);
+		
+	}
+
+}
